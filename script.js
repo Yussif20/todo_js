5d9e2552852e36bad1d2b0997f8716e8c5213283
Yousef ayman <yousefayman2000500@gmail.com>
2024-08-19 22:28:57 +0300

    adding the delte and check functions

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const tasksContainer = document.querySelector(".tasks");
 
 const createTask = (task)=>{
     let taskEl = `
-        <li role="button" class="task">${task}</li>
+        <li role="button" class="task">${task}<span>\u00d7</span></li>
     `
     return taskEl;
 }
@@ -14,6 +14,30 @@ const addTaskHandler = ()=>{
                     window.alert("You must write something!"):
                     tasksContainer.insertAdjacentHTML("afterbegin",createTask(inputBox.value));
     inputBox.value = '';
+    saveData();
 }
 
 addButton.addEventListener("click",addTaskHandler);
+window.addEventListener("keypress",(e)=>{
+    e.code === "Enter" && addButton.click();
+})
+
+
+tasksContainer.addEventListener("click",(e)=>{
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked") ;
+        saveData();
+    } 
+    if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove() ;
+        saveData();
+    }
+})
+
+const saveData = ()=>{
+    localStorage.setItem("data",tasksContainer.innerHTML)
+}
+const showData = ()=>{
+     tasksContainer.innerHTML = localStorage.getItem("data");
+}
+showData();
